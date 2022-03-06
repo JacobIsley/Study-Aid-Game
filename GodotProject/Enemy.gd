7e38f3c60f1bbe01c2643b7013163bf4c2e2617f
yluther <yanyejoy@gmail.com>
2022-03-06 15:59:00 -0700

    tutorial #3

--- GodotProject/Enemy.gd
+++ GodotProject/Enemy.gd
@@ -3,7 +3,18 @@ extends Node2D
 var hp = 10 setget set_hp
 
 onready var hpLabel = $"HP Label"
+onready var animationPlayer = $AnimationPlayer
+
+signal died
 
 func set_hp(new_hp):
 	hp = new_hp
 	hpLabel.text = str(hp) + "hp"
+	
+	if hp <= 0:
+		omit_signal("died")			# error here "the method omit_signal isn't declared in the current class
+		queue_free()
+	else:
+		animationPlayer.play("Slash")	# I called it slash instead of shake because initially I thought that's what I was animating
+		yield(animationPlayer, "animation_finished")
+		animationPlayer.play("Attack")
